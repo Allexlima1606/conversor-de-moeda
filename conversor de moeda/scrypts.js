dfc1be408064a1d2b3b490fed93818ff0c7ba9ba
Allexlima1606 <allex.z@hotmail.com>
2023-04-20 11:23:01 -0300

    first commit

---
+++ conversor de moeda/scrypts.js
@@ -0,0 +1,45 @@
+const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
+
+const dolar = 5.2
+const euro = 5.9
+
+const convertValues = () => {
+    const inputReais = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+    const currencyValuTest = document.getElementById('currency-valu-test')
+
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: 'BRL' }
+    ).format(inputReais)
+
+    if (select.value === "US$ Dolar Americano")
+        currencyValuTest.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(inputReais / dolar)
+
+    if (select.value === "€ Euro")
+        currencyValuTest.innerHTML = new Intl.NumberFormat('en-DE',
+            { style: 'currency', currency: 'EUR' }
+        ).format(inputReais / euro)
+}
+
+changeCurrency = () => {
+    const currencyName = document.getElementById('currency-name')
+    const currencyImg = document.getElementById('currency-img')
+
+    if (select.value === 'US$ Dolar Americano') {
+        currencyName.innerHTML = "Dolar Americano"
+        currencyImg.src = "./assets/estados-unidos.png"
+    }
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assets/euro.png"
+    }
+
+    convertValues()
+}
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeCurrency)
